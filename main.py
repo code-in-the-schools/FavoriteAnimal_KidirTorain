1a765ea62e0ad3bd262c46a853b7e34a37fe1ef1
Kidir Torain <kktorainnn@gmail.com>
2020-07-22 15:42:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# spent 70% of their lives sleeping
+# walks like camels and giraffes
+# oldest lived 9,500 years ago
+# mayor of an Alaskan town for 20 years
+c = int(input())
+if c == 1:
+  print(" spent 70% of their lives sleeping ")
+elif c == 2:
+  print(" walks likes camels and giraffes ")
+elif c == 3:
+  print("oldest lived 9,500 years ago")
+else 
+  print("mayor of an Alaskan town for 20 years")
+
+
+
+
